62185cfafd73f480d315077a8789d37ce2756bb4
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-07-17 23:39:45 +0200

    first commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React  from "react";
+
+export default function Welcome() {
+  return (
+    <div className="welcome-warpper">
+      <h1 className="welcome-title">Quizzical</h1>
+      <p>Some description if needed</p>
+      <button className="start-Btn">Start Quiz</button>
+    </div>
+  );
+}
