19e9959e7880d48ac597b5f8e3083a1eeaccf1da
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-07-20 20:02:55 +0200

    update the ui
    update the check btn and <p> for the number of correct answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,60 @@
+import React from "react";
+import { nanoid } from "nanoid";
 
+export default function Question(props) {
+  const answers = props.allAnswers.map((answer) => {
+    return {
+      id: nanoid(),
+      answer: answer,
+      isChoosen: false,
+    };
+  });
 
-export default function Question(props){
+  const [answersState, setAnswersState] = React.useState(answers);
+
+  function choose(id) {
+    console.log(props.hasEnded)
+    props.hasEnded && setAnswersState((prevAnswersState) => {
+      return prevAnswersState.map((answerState) => {
+       
+        if (id === answerState.id) {
+          return {
+            ...answerState,
+             isChoosen: !answerState.isChoosen
+          }
+        }else{
+            {
+                return {...answerState,isChoosen: false}
+            }
+        }
+      });
+    }
     
+    
+    
+    
+    );
+  }
 
 
 
-
-
-    return(
-        <main className="qustion-item">
-        <h2 className="question-text">{props.question}</h2>
-        <ul className="options-list">
-          <li onClick={choose}>{props.correctAnswer}</li>
-          <li onClick={choose}>{props.wrongAnswers[0]}</li>
-          <li onClick={choose}>{props.wrongAnswers[1]}</li>
-          <li onClick={choose}>{props.wrongAnswers[2]}</li>
-        </ul>
-        <hr/>
-      </main>
-    )
+  return (
+    <main className="qustion-item">
+      <h2 className="question-text">{props.question}</h2>
+      <ul className="options-list">
+        {answersState.map((answer) => {
+          return (
+            <li
+              key={answer.id}
+              onClick={() => choose(answer.id)}
+              style={{backgroundColor: answer.isChoosen? "#D6DBF5" : "#fff"}}
+            >
+              {answer.answer}
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+    </main>
+  );
 }
--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,36 +1,56 @@
 import React, { useEffect } from "react";
-import Question  from "./Question";
+import Question from "./Question";
+import { nanoid } from "nanoid";
 
 export default function QuizPage() {
+  const [allQuestions, setAllQuestions] = React.useState([]);
+  const [hasEnded , setHasEnded] = React.useState(false)
 
-  // const [question, setQuestion] = React.useState({
-  //   question: "",
-  //   correctAnswer: "",
-  //   wrongAnswers: [] 
-//})
+  useEffect(() => {
+    newQuiz ()
+  }, []);
 
-  const [allQuestions, setAllQuestions] = React.useState([])
-  
-  
-useEffect(() =>{
-  fetch('https://opentdb.com/api.php?amount=5&type=multiple&difficulty=easy')
-  .then(res =>  res.json())
-  .then(data => setAllQuestions(data.results))
- 
-} , [])
+  function newQuiz (){
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    .then((res) => res.json())
+    .then((data) =>
+      setAllQuestions(
+        data.results.map((item) => {
+          return {
+            id: nanoid(),
+            question: item.question,
+            correctAnswer: item.correct_answer,
+            answers: [...item.incorrect_answers, item.correct_answer],
+          };
+        })
+      )
+    );
+    setHasEnded(prevState => !prevState)
+ console.log(hasEnded)
+  }
 
 
+function checkAnswers (){
+  setHasEnded(prevState => !prevState)
+}
 
   return (
     <main>
-      {allQuestions.map(question => {
-return <Question
-question = {question.question}
-correctAnswer = {question.correct_answer}
-wrongAnswers = {question.incorrect_answers}/>
+      <h1>Quizzical</h1>
+      {allQuestions.map((question) => {
+        return (
+          <Question
+            key={question.id}
+            question={question.question}
+            correctAnswer={question.correctAnswer}
+            allAnswers={question.answers}
+            hasEnded= {hasEnded}
+          />
+        );
       })}
-
+     {hasEnded  && <p>Correct Answers (5/10)</p>}
+      <button className="check-answersBtn" onClick={hasEnded? checkAnswers : newQuiz }>
+      { hasEnded?"New game" : "Check Answers"}</button>
     </main>
-  
   );
 }
